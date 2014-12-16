7227886c8668889f77f20df814f9534c6b01f79c
kensuketamura <kensuke.tamura.183@gmail.com>
2014-12-16 13:52:53 +0900

    delete unnecessary files

--- public/js/visual.js
+++
@@ -1,4 +0,0 @@
-///<reference path="../../typings/jquery/jquery.d.ts" />
-$(function () {
-    createNodeViewFromP4DJson(JSON.parse(source));
-});
--- public/js/visual.ts
+++
@@ -1,8 +0,0 @@
-///<reference path="../../typings/jquery/jquery.d.ts" />
-
-declare var source
-declare var createNodeViewFromP4DJson
-
-$(function(){
-  createNodeViewFromP4DJson(JSON.parse(source));
-});
